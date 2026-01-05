d9647a591d9119f08245cf2a1a9a94c220ba91f2
Kriyesha2004 <chathumik2004@gmail.com>
2026-01-05 20:57:22 +0530

    nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="Experience">Experience</a></li>
+        <li><a href="#experience">Experience</a></li>
         <li><a href="#contact">Contact</a></li>
 
       </ul>
